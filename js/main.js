dbbb1432c0fc6bc289c696790287571d36b6815a
Sean Swan <ogpswan@YAHOO.COM>
2018-12-03 03:48:53 -0800

    adding javascript for questions array finished addind inputs to scss for now

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-
+// Questions Array
+const questions = [
+  { question: 'Enter Your First Name'},
+  { question: 'Enter Your Last Name'},
+  { question: 'Enter Your Email Name'},
+  { question: 'Create A Password'},
+]
